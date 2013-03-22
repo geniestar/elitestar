4c6e46e903fee1d424e5bade1d48ed320be5e6a7
geniestar <92401013@nccu.edu.tw>
2013-03-22 01:39:40 +0000

    finish prepare statement

--- test.php
+++ test.php
@@ -3,27 +3,48 @@ $mysqli = new mysqli('localhost', 'root', 'elitestar', 'elitestar');
 
 //$sql = 'SELECT * FROM users WHERE id =? AND phone=?';
 $sql = 'INSERT INTO users VALUE(?, ?, ?, ?, ?, ?)';
-$id = 'testaccount3';
+$id = 'testaccount4';
 $phone = '0925083472';
 $pw = 'cc03e747a6afbbcbf8be7668acfebe';
 $name = 'Kaeson Ho';
 $mail = 'test@yahoo.com';
 $role = '1';
 
-$preparedParams = array(&$id, &$pw, &$name, &$mail, &$phone, &$role);
+$inputParams = array('testaccount5', 'cc03e747a6afbbcbf8be7668acfebe', 'Kaeson Ho', '0925083472', 'test@yahoo.com', 1);
 
+function refValues($arr)
+{
+    if (strnatcmp(phpversion(),'5.3') >= 0) //Reference is required for PHP 5.3+
+    {
+        $refs = array();
+        foreach($arr as $key => $value)
+        {
+            $refs[$key] = &$arr[$key];
+        }
+        return $refs;
+    }
+    return $arr;
+}
 
 if ($statement = $mysqli->prepare($sql))
 {
-    $type = str_repeat('s', count($params));
-    if (!empty($params))
+    /*process input params*/
+    $preparedParams  = array();
+    foreach($inputParams as $inputParam)
     {
-        call_user_func_array(array($statement, 'bind_param'), array_merge((array)$type, $params));
+        $preparedParams[] = $inputParam;
+    }
+
+    /*bind params*/
+    $type = str_repeat('s', count($preparedParams));
+    if (!empty($preparedParams))
+    {
+        call_user_func_array(array($statement, 'bind_param'), array_merge((array)$type, refValues($preparedParams)));
     }
 
     $meta = $statement->result_metadata();
 
-    /* define fileds*/
+    /* define result fileds*/
     $results = array();
     while ($meta && $field = $meta->fetch_field())
     {
@@ -33,6 +54,7 @@ if ($statement = $mysqli->prepare($sql))
     {
         call_user_func_array(array($statement, 'bind_result'), $results);
     }
+
     $statement->execute();
 
     /*fetch the results*/
@@ -42,6 +64,5 @@ if ($statement = $mysqli->prepare($sql))
     }
     $statement->close();
 }
-
 
 ?>
