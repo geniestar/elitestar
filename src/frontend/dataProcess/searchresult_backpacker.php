f8b9ec4b48a107ebfa90b6c546667f599499f386
geniestar <92401013@nccu.edu.tw>
2013-04-17 17:06:34 +0000

    find backpacker layout

--- src/frontend/dataProcess/searchresult_backpacker.php
+++ src/frontend/dataProcess/searchresult_backpacker.php
@@ -10,4 +10,69 @@ else
 
 $states = ConfigReader::getInstance()->readConfig('dimensions', 'states');
 $data['place'] = $states[$data['backpacker']['state']]['name'] . ' , ' . $states[$data['backpacker']['state']]['suburbs'][$data['backpacker']['city']];
+$data['arrivalTime'] = date('d/M/Y', strtotime($data['backpacker']['arrival_time']));
+
+$duration = strtotime($data['backpacker']['duration_end']) - strtotime($data['backpacker']['duration_start']);
+$durationText = '';
+$month = floor($duration/(60*60*24*30));
+
+if ($month > 12)
+{
+    $year = floor($month/12);
+    $month = $month - $year*12;
+}
+
+if ($year)
+{
+    $durationText .= sprintf(EliteHelper::getLangString('SEARCH_RESULT_DURATION_YEAR'), $year);
+}
+
+if ($month)
+{
+    $durationText .= sprintf(EliteHelper::getLangString('SEARCH_RESULT_DURATION_MONTH'), $month);
+}
+if (!$durationText)
+{
+    $durationText = EliteHelper::getLangString('SEARCH_RESULT_DURATION_LESS_THAN_MONTH');
+}
+
+$data['durationText'] = $durationText;
+
+$bedText = EliteHelper::getLangString('COMMON_SEARCH_BEDS_S') . $data['backpacker']['beds_single'] . ' ' . EliteHelper::getLangString('COMMON_SEARCH_BEDS_D') . $data['backpacker']['beds_double'];
+$data['bedText'] = $bedText;
+
+$additionalHelpArray = array();
+$checkArray = array('ha', 'hb', 'hc', 'hd');
+$additionalHelps = json_decode(json_decode($data['backpacker']['additional_help']), true);
+
+foreach ($checkArray as $checkIndex)
+{
+    if ($additionalHelps[$checkIndex])
+    {
+        $additionalHelpArray[] = array(
+            'class' => $checkIndex,
+            'additional' => isset($additionalHelps[$checkIndex . 'a'])?$additionalHelps[$checkIndex . 'a']:''
+        );
+    }
+}
+
+$data['additionalHelpArray'] = $additionalHelpArray;
+
+$facilitiesArray = array();
+$checkArray = array('fa', 'fb', 'fc', 'fd', 'fe', 'ff', 'fg');
+$facilities = json_decode(json_decode($data['backpacker']['facilities']), true);
+
+foreach ($checkArray as $checkIndex)
+{
+    if ($facilities[$checkIndex])
+    {
+        $facilitiesArray[] = array(
+            'class' => $checkIndex,
+            'additional' => isset($facilities[$checkIndex . 'a'])?$facilities[$checkIndex . 'a']:''
+        );
+    }
+}
+
+$data['facilitiesArray'] = $facilitiesArray;
+
 ?>
