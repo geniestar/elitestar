f317a555cf3096e4393c8823663e1546b375bea2
geniestar <92401013@nccu.edu.tw>
2013-04-26 16:28:42 +0000

    add css

--- src/frontend/html/admin.php
+++ src/frontend/html/admin.php
@@ -5,25 +5,43 @@ include('/usr/share/pear/elitestar/lib/BackPackers.php');
 include('/usr/share/pear/elitestar/lib/LandLords.php');
 include('/usr/share/pear/elitestar/lib/ContentGenerator.php');
 $user = EliteUsers::getInstance()->getCurrentUser();
-if ('basic' == $_GET['type'])
+if (0 == $user['role'])
 {
-
+    $role = 'houseowner';
 }
-else if ('settings' == $_GET['type'])
+else
 {
-
+    $role = 'backpacker';
 }
-else if ('messages' == $_GET['type'])
+if ('basic' == $_GET['action'])
 {
-
+    $tabs = array(array('class' => 'basic-info', 'name' => EliteHelper::getLangString('COMMON_MENU_BASIC_INFO')));
 }
-else if ('profit' == $_GET['type'])
+else if ('settings' == $_GET['action'])
 {
-
+    if (0 == $user['role'])
+    {
+        $tabs = array(
+            array('class' => 'settings', 'name' => EliteHelper::getLangString('COMMON_MENU_SETTINGS')),
+            array('class' => 'service', 'name' => EliteHelper::getLangString('COMMON_MENU_SERVICE')),
+        );
+    }
+    else
+    {
+        $tabs = array(array('class' => 'settings', 'name' => EliteHelper::getLangString('COMMON_MENU_SETTINGS')));
+    }
 }
-else if ('suggestion' == $_GET['type'])
+else if ('messages' == $_GET['action'])
 {
-
+    $tabs = array(array('class' => 'messages', 'name' => EliteHelper::getLangString('COMMON_MENU_MESSAGES')));
+}
+else if ('profit' == $_GET['action'])
+{
+    $tabs = array(array('class' => 'profits', 'name' => EliteHelper::getLangString('COMMON_MENU_PROFITS')));
+}
+else if ('suggestion' == $_GET['action'])
+{
+    $tabs = array(array('class' => 'suggestion', 'name' => EliteHelper::getLangString('COMMON_MENU_SUGGESTION')));
 }
 $headData = array(
     'title' => EliteHelper::getLangString('COMMON_B_TITLE'),
@@ -46,18 +64,23 @@ $tailData = array(
         <?php EliteHelper::initJsObject();?>
         <?php echo ContentGenerator::getContent('head', $headData);?>
     </head>
-    <body>
+    <body class="<?php echo $role;?>">
         <?php echo ContentGenerator::getContent('common_banner', array('title' => EliteHelper::getLangString('COMMON_B_TITLE')));?>
         <?php echo ContentGenerator::getContent('common_menu', array('user' => $user));?>
         <div class="main-container">
-            <div class="col-left-left-big col">
+            <div class="col-combined col">
                 <div class="row">
                     <?php echo ContentGenerator::getContent('common_adminmenu', array());?>
                 </div>
             </div>
-            <div class="col-right-left-big col">
+            <div class="col-left-left-big col">
+                <div class="row">
+                    <?php echo ContentGenerator::getContent('common_admintab', array('tabs' => $tabs));?>
+                </div>
                 <div class="row">
                 </div>
+            </div>
+            <div class="col-right-left-big col">
                 <div class="row">
                 </div>
             </div>
